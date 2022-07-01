a8894e7b9cf3151019a79a3c4a18ec9125845c64
Ryan Sadwick <rsadwick@gmail.com>
2022-06-30 22:34:52 -0400

    path is now validated and defaults to application folder

--- Program.cs
+++ Program.cs
@@ -1,29 +1,28 @@
 ﻿using System;
 using System.IO;
-namespace FileRenamerPro
-{
-    internal static class Program
-    {
-        private static void Main(string[] args) {
-            string title = @"
+namespace FileRenamerPro {
+    internal static class Program {
+        private static string title = @"
   _____.__.__                                                                                    
 _/ ____\__|  |   ____   _______   ____   ____ _____    _____   ___________  _____________  ____  
 \   __\|  |  | _/ __ \  \_  __ \_/ __ \ /    \\__  \  /     \_/ __ \_  __ \ \____ \_  __ \/  _ \ 
  |  |  |  |  |_\  ___/   |  | \/\  ___/|   |  \/ __ \|  Y Y  \  ___/|  | \/ |  |_> >  | \(  <_> )
  |__|  |__|____/\___  >  |__|    \___  >___|  (____  /__|_|  /\___  >__|    |   __/|__|   \____/ 
                     \/               \/     \/     \/      \/     \/        |__|                 ";
-            var path = "";
-            var prepend = "";
-            for (var i = 0; i < args.Length; i++){
+        private static string path = "";
+        private static string prepend = "";
+        private static void Main(string[] args) {
+            Console.WriteLine(title);
+            for (var i = 0; i < args.Length; i++) {
                 if (args[i] == "-p") {
                     path = args[i + 1];
                 }
 
-                if (args[i] == "-n"){
+                if (args[i] == "-n") {
                     prepend = args[i + 1];
                 }
 
-                if(args[i] == "-h" || args[i] == "-help") {
+                if (args[i] == "-h" || args[i] == "-help") {
                     Console.WriteLine(title);
                     Console.WriteLine("-p   :   path to files to be renamed.");
                     Console.WriteLine("-n   :   name to prepend to filename.  eg. '-n hi_ for hi_filename.txt.");
@@ -31,17 +30,18 @@ _/ ____\__|  |   ____   _______   ____   ____ _____    _____   ___________  ____
                 }
             }
 
-            if(string.IsNullOrEmpty(path)) {
-                Console.WriteLine(title);
-                Console.WriteLine("No path provided.  Please provide -p {path}.  For example: c:\\myFolder\\files.  -h for help. ");
-                return;
+            var isPathValid = validatePath();
+            if(isPathValid){
+                rename();
             }
+        }
 
-            var directoryInfo = new DirectoryInfo(@"" + path);
+        private static void rename() {
+            var directoryInfo = new DirectoryInfo(@"" + path); 
             var files = directoryInfo.GetFiles();
-            Console.WriteLine(title);
 
-            foreach (var info in files) {   
+            foreach (var info in files)
+            {
                 var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(info.Name);
                 var newFilename = $"{prepend}{fileNameWithoutExtension}{info.Extension}";
                 var cleanFileName = newFilename.Replace(" ", "").Replace("(", "").Replace(")", "");
@@ -49,8 +49,37 @@ _/ ____\__|  |   ____   _______   ____   ____ _____    _____   ___________  ____
 
                 File.Move(info.FullName, newFullFilename);
 
-                Console.WriteLine("Renaming: {0} -> {1}", info.FullName, newFullFilename);
+                Console.WriteLine("Renaming: {0} -> {1}", info.Name, newFullFilename);
             }
+            Console.WriteLine("Total files renamed: {0}", files.Length);
+        }
+
+        private static bool validatePath(){
+            if (string.IsNullOrEmpty(path)) {
+                var pathToApplication = System.AppDomain.CurrentDomain.BaseDirectory;
+                var useDefaultPathDisplay = $"No path provided.  Use local path: " + $"{pathToApplication} instead?";
+                var useDefaultPath = UtilsConsole.Confirm(useDefaultPathDisplay);
+                if (useDefaultPath) {
+                    path = pathToApplication;
+                    return true;
+                } 
+                else {
+                    Console.WriteLine("Please enter path that contains files to be renamed:");
+                    path = Console.ReadLine();
+                    if(string.IsNullOrEmpty(path) || !Path.IsPathRooted(path)){
+                        var tryPathAgain = UtilsConsole.Confirm("Not a valid path, try again?");
+                        if(tryPathAgain){
+                            path = "";
+                            validatePath();
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
